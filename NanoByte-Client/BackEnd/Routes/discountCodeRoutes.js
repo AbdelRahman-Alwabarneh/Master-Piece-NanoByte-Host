403e749676d84359186935f66f47824f7cbbf995
AbdelRahman-Alwabarneh <abdelrahmanalwabarneh@gmail.com>
2025-02-20 14:13:45 +0300

    Modify the path of the discount code file

--- NanoByte-Client/BackEnd/Routes/discountCodeRoutes.js
+++ NanoByte-Client/BackEnd/Routes/discountCodeRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const discountCodeControllers = require('../Controllers/discountCodeControllers');
+const discountCodeControllers = require('../Controllers/DiscountCode/discountCodeControllers');
 const userIdMiddleware = require('../Middlewares/userIdMiddleware');
 
 router.post('/', userIdMiddleware,discountCodeControllers.DiscountCodeData);
